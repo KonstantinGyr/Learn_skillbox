31a2df6661ee4eedfe9555ae191a3a4c93be3734
Konstantin Guranovski <gyranovski@mail.ru>
2022-02-19 14:18:16 +0400

    modif1

--- search_word/main.cpp
+++ search_word/main.cpp
@@ -3,18 +3,37 @@
 #include <vector>
 #include <string>
 
+std::string rightAdd( std::string& add){
+    std::string newAdd;
+    for(char symbol : add) {
+        if (symbol == '\\'){
+            newAdd.push_back('\\');
+        }
+        newAdd.push_back(symbol);
+    }
+    return newAdd;
+}
+
 int main()
 {
     std::ifstream text;
-    std::string w;
-    text.open("C:\\text.txt");
+    std::string word;
+    std::string address;
+    do {
+        std::cout << "Input address your file : " << std::endl;
+        std::cin >> address;
+        text.open(rightAdd(address));
+        std::cout << ((text.is_open()) ? "Ok address " : "Not correct address ");
+        std::cout << std::endl;
+    }
+    while(!text.is_open());
     std::cout << "Input search word :  ";
     std::string search;
     std::cin>>search;
     int count=0;
     while(!text.eof()) {
-        text >> w;
-        if (w == search) {
+        text >> word;
+        if (word == search) {
             count++;
         }
     }
